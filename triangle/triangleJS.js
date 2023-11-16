3a38709a6d841e1637f96ad892816e3b64621048
TejDeepM <tejdeepm30@gmail.com>
2023-11-16 07:36:16 +0400

    Triangle area finished

--- triangle/triangleJS.js
+++ triangle/triangleJS.js
@@ -1,15 +1,18 @@
 
-let heronsAreaButton = document.getElementById("calculateButton")
-heronsAreaButton.addEventListener("click", calculateAreaHerons)
-
 function calculateAreaHerons() {
     var a = parseFloat(document.getElementById("sideOne").value)
     var b = parseFloat(document.getElementById("sideTwo").value)
     var c = parseFloat(document.getElementById("sideThree").value)
     var s = ((a + b + c)/2).toFixed(2)
-    console.log(s)
 
     var area = Math.sqrt(s*(s-a)*(s-b)*(s-c)).toFixed(2)
-    console.log(area)
-    document.getElementById("heronsAnswer").innerHTML = area
+    document.getElementById("heronsAnswer").innerHTML = area.toString()
+}
+
+function calculateAreaHalfbh() {
+    var b = parseFloat(document.getElementById("base").value).toFixed(2)
+    var h = parseFloat(document.getElementById("height").value).toFixed(2)
+    var area = (b*h)/2
+
+    document.getElementById("halfbhAnswer").innerHTML = area.toString()
 }
